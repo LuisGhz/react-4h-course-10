4c6de99db795a15c6392c711bd9d15d9d3f2137e
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-09 14:49:20 -0500

    Create form and set values with useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
   return (
     <div className="App">
-      
+      <form action="">
+        <div>
+          <label>Name</label>
+          <input 
+            value={ name }
+            onChange={ e => setName(e.target.value) }
+            type="text" 
+          />
+        </div>
+        <div>
+          <label>Email</label>
+          <input 
+            value={ email }
+            onChange={ e => setEmail(e.target.value) }
+            type="text" 
+          />
+        </div>
+      </form>
     </div>
   );
 }
